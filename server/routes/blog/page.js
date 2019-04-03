e65ec580201487519ca842fe98554eabc4e502dd
吴晓权 <somerq@qq.com>
2019-04-04 02:57:00 +0800

    feature:调整文章列表顺序

--- server/routes/blog/page.js
+++ server/routes/blog/page.js
@@ -10,13 +10,13 @@ router.post('/list', async(ctx) => {
     const Page = mongoose.model('Page')
     let pageData = []
     if (id) {
-      pageData = await Page.find({isDelete: false, category: id}).populate('author').populate('category').populate('comment')
+      pageData = await Page.find({isDelete: false, category: id}).sort({'_id': -1}).populate('author').populate('category').populate('comment')
       // console.log('idpage', pageData)
     } else {
       if (page) currentPage = page
       const skipCount = Number(currentPage) * 5
       console.log(skipCount)
-      pageData = await Page.find({isDelete: false}).skip(skipCount).limit(5).populate('author').populate('category').populate('comment')
+      pageData = await Page.find({isDelete: false}).sort({'_id': -1}).skip(skipCount).limit(5).populate('author').populate('category').populate('comment')
       console.log('nopage', pageData)
     }
     ctx.body = {
@@ -40,7 +40,7 @@ router.get('/', async(ctx) => {
     const { id } = ctx.request.query
     // console.log(id)
     const Page = mongoose.model('Page')
-    const pageData = await Page.findOne({_id: id})
+    const pageData = await Page.findOne({_id: id}).populate('author').populate('category').populate('comment')
     ctx.body = {
       status: 200,
       result: pageData,
